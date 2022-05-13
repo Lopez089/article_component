25318ab5d50f5cf9a016ca1e35e8c452df1d001d
LopezDev <aragonlopezjuan87@gmail.com>
2022-05-13 18:47:50 +0200

    feat: create card toogle to shere

--- main.js
+++ main.js
@@ -1,36 +1,63 @@
 // Ver en que tipo de pantalla estoy
-// si estoy en pantalla grande o tablet 
-//si es grande
-// insertar el componente en el html
-// todo: medir en que pixeles esta el boton shere
-// apartir de ahi poner eso pixele con css para poner el componete en su sitio
-// si es movil
-// si el usuario hace click en shere
-// agregar el componente en la parte de footer
-// quitar toda la parte del footer anterior para
-// dar estilos 
+//---- si estoy en pantalla grande o tablet 
+//--------si es grande
+//-----------agregar un evento de click 
+// ---------------si hace click
+// ------------------insertar el componente en el html
+// -------si es movil
+//-----------agregar un evento de click 
+// ---------------si hace click
+// ------------------insertar el componente en el html
+// ------------------agregar los estilos para movil
+
 
 const $cardShare = document.querySelector('.card-share')
+
 const resolution = window.screen.width
 const componentTooltip = `
     <div class="card-tooltip">
         <span class="triangulo-shere"></span>
         <p>SHERE</p>
         <section class="card-social">
-            <a href="#">
+            <a href="">
                 <img src="static/icon-facebook.svg" alt="face">
             </a>
-            <a href="#">
+            <a href="">
                 <img src="static/icon-twitter.svg" alt="twitter">
             </a>
-            <a href="#">
+            <a href="">
                 <img src="static/icon-pinterest.svg" alt="pinterest">
             </a>
         </section>  
     </div>`
+const addComponentCardShere = () => $cardShare.insertAdjacentHTML('afterbegin', componentTooltip)
+
+
+
 
 if (resolution < 768) {
-    //alert('movil')
+    $cardShare.insertAdjacentHTML('afterbegin', componentTooltip)
+    const $cardTooltip = document.querySelector('.card-tooltip')
+
+    const toogleVisivility = () => {
+        if ($cardTooltip.style.visibility === 'hidden') {
+            $cardTooltip.style.visibility = 'visible'
+        } else {
+            $cardTooltip.style.visibility = 'hidden'
+        }
+    }
+    $cardShare.addEventListener('click', () => toogleVisivility())
 } else {
     $cardShare.insertAdjacentHTML('afterbegin', componentTooltip)
+    const $cardTooltip = document.querySelector('.card-tooltip')
+
+    const toogleVisivility = () => {
+        if ($cardTooltip.style.visibility === 'hidden') {
+            $cardTooltip.style.visibility = 'visible'
+        } else {
+            $cardTooltip.style.visibility = 'hidden'
+        }
+    }
+    $cardShare.addEventListener('click', () => toogleVisivility())
+
 }
